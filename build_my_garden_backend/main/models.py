48926971d0e81547105e6f61f0a2f220aa256f13
Deepak <dkrishnaagovindarajan21@ole.augie.edu>
2022-06-26 22:16:39 -0500

    soil and soil type models

--- build_my_garden_backend/main/models.py
+++ build_my_garden_backend/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Soil type model
+class SoilType(models.Model):
+    soil_name = models.CharField(max_length=200)
+    soil_description = models.TextField(max_length=200)
+    soil_degradation_duration = models.DurationField()
+
+# Soil model
+class Soil(models.Model):
+    # Account ID
+    account_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='soil_account', null=True)
+    soil_type_id = models.ForeignKey(SoilType, on_delete=models.CASCADE, related_name='soil_type', null= True)
+    soil_current_degradation = models.FloatField()
