d0da582ede38291c513e43d0207da9c86aac5f89
Deepak Krishnaa Govindarajan <77573925+deepakkrish212@users.noreply.github.com>
2022-07-09 17:58:53 -0500

    Update settings.py

--- build_my_garden_backend/build_my_garden_backend/settings.py
+++ build_my_garden_backend/build_my_garden_backend/settings.py
@@ -174,7 +174,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES' : [
         'rest_framework.permissions.IsAuthenticated',
         'rest_framework.authentication.TokenAuthentication',
-         main
+         'main',
     ],
 
     # Authentication for
