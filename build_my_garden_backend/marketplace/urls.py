3ecd3aa23a9618c508b75db243d8488f70a7405b
abemelech <abemelech.mesfin@gmail.com>
2022-07-14 16:28:57 -0500

    created the endpoint for the search

--- build_my_garden_backend/marketplace/urls.py
+++ build_my_garden_backend/marketplace/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ImageAPI, ListingView, SellerInfoAPI, SellerListing
+from .views import ImageAPI, ListingSearchView, ListingView, SellerInfoAPI, SellerListing
 
 urlpatterns = [
     path("listing/", ListingView.as_view()),
     path("image/add/", ImageAPI.as_view()),
     path("seller/", SellerInfoAPI.as_view()),
     path("seller/listing/", SellerListing.as_view()),
+    path("listing/search/", ListingSearchView.as_view()),
 ]
