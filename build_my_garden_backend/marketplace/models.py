656dab761d9f5577881c5fe5df78dc4c9017410c
abemelech <abemelech.mesfin@gmail.com>
2022-07-20 00:01:12 -0500

    wrong import removed

--- build_my_garden_backend/marketplace/models.py
+++ build_my_garden_backend/marketplace/models.py
@@ -1,4 +1,3 @@
-from distutils.command.upload import upload
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from location_field.models.plain import PlainLocationField
