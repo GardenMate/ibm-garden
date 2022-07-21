dddd39923295f58452a6cc6bba557aebb00c96b3
Abemelech <abemelech.mesfin@gmail.com>
2022-07-20 23:55:04 -0500

    Temp save

--- build_my_garden/lib/pages/navpages/DetailsPage.dart
+++ build_my_garden/lib/pages/navpages/DetailsPage.dart
@@ -1,25 +1,46 @@
+import 'dart:html';
+
+import 'package:build_my_garden/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 
 class DetailsPage extends StatelessWidget {
-  
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            flexibleSpace: FlexibleSpaceBar(
-              background: Image.asset(
-                ""),
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(20),
+            child: Container(
+              color: Colors.white,
+              child: Center(
+                  child: AppText(
+                text: "Sustainable ming Practices",
+                size: 30,
+              )),
+              width: double.maxFinite,
+              padding: EdgeInsets.only(top: 5, bottom: 10),
             ),
-
-          )
-        ],
-      )
-    );
-    
+          ),
+          pinned: true,
+          backgroundColor: Color.fromARGB(255, 255, 228, 182),
+          expandedHeight: 300,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Image.asset(
+              "assets/images/CategoryPageImg/img5.gif",
+              width: double.maxFinite,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            child: Text(
+                "Through decades of science and practice, the following farming practices have proven effective in achieving sustainability, especially when used in combination:Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations.Rotating crops and embracing diversity. Planting a variety of crops can have many benefits, including healthier soil and improved pest control. Crop diversity practices include intercropping (growing a mix of crops in the same area) and complex multiyear crop rotations."),
+          ),
+        )
+      ],
+    ));
   }
 }
--- build_my_garden/lib/pages/navpages/categorylistpage.dart
+++ build_my_garden/lib/pages/navpages/categorylistpage.dart
@@ -20,142 +20,114 @@ class CategoryListPage extends StatefulWidget {
 }
 
 class _CategoryListPageState extends State<CategoryListPage> {
-  List <Category> categories = Utils.getMockedCategories(); 
-
+  List<Category> categories = Utils.getMockedCategories();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Drawer(),
       body: Container(
-        child: Column( 
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
               padding: const EdgeInsets.only(top: 20, bottom: 10, left: 30),
-              child: AppLargeText(text: "Categories",)
-            ),
-            Expanded(
+              child: AppLargeText(
+                text: "Categories",
+              )),
+          Expanded(
               child: ListView.builder(
-                itemCount: categories.length,
-                itemBuilder: (BuildContext ctx, int index ) {
-                  return Container(
-                    margin: EdgeInsets.all(20),
-                    height: 150,
-                    child: Stack(
-                      clipBehavior: Clip.none,
-                    
-                      children: [
-                        Positioned.fill(
-                          
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: Image.asset('assets/images/CategoryPageImg/' + categories[index].imgName + '.gif',
-                            fit: BoxFit.cover,
-                            
-                                
-                            width: displayWidth(context),),
-                            ) 
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          right: 0,
-                          left: 0,
-                          child: Container(
-                            height: 150,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              gradient: LinearGradient(
-                                begin: Alignment.bottomCenter,
-                                end: Alignment.topCenter, 
-                               colors: [
-                                Colors.black.withOpacity(0.8),
-                                Colors.transparent,
-                               ])
-                            ),
-                    
-                          )
-                        ),
-                        Positioned(
-                    
-                           bottom: 0,
-                           right: 0,
-                           left: 0,
-                         
-                        
-                          child: Container(
-                             height: 150,
-                             decoration: BoxDecoration( 
-                              borderRadius: BorderRadius.circular(20), 
-                              color: AppColorsList[index],
-                              // borderRadius: BorderRadius.only(
-                              // bottomLeft: Radius.circular(20),
-                              // bottomRight: Radius.circular(20),
-                              // ),
-                              // the color on top of the boxes
-                            )
-                    
-                          ) 
-                                               
-                        ),
-                        Positioned(
-                          child: Padding(
-                            padding: const EdgeInsets.all(15),
-                            child: Row(
+                  itemCount: categories.length,
+                  itemBuilder: (BuildContext ctx, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => DetailsPage(),
+                        ));
+                      },
+                      child: Container(
+                        margin: EdgeInsets.all(20),
+                        height: 150,
+                        child: Stack(
+                          clipBehavior: Clip.none,
                           children: [
-                            Container(
-                              child: Text(
-                                categories[index].name,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.bold,
-                    
-                                 ),
+                            Positioned.fill(
+                                child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                'assets/images/CategoryPageImg/' +
+                                    categories[index].imgName +
+                                    '.gif',
+                                fit: BoxFit.cover,
+                                width: displayWidth(context),
                               ),
-                            )
-                            
-                    
-                          ],
-                        )
-                        
-                        )
-                        ),
-                        Positioned(
-                          height: 150,
-                          
-                            child: GestureDetector(
-                              onTap: (){
-                             Navigator.of(context).push(MaterialPageRoute(builder: (context) => DetailsPage(),));
-                                          },
-                              child: Row(
-
-                                
-                            
+                            )),
+                            Positioned(
+                                bottom: 0,
+                                right: 0,
+                                left: 0,
+                                child: Container(
+                                  height: 150,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      gradient: LinearGradient(
+                                          begin: Alignment.bottomCenter,
+                                          end: Alignment.topCenter,
+                                          colors: [
+                                            Colors.black.withOpacity(0.8),
+                                            Colors.transparent,
+                                          ])),
+                                )),
+                            Positioned(
+                                bottom: 0,
+                                right: 0,
+                                left: 0,
+                                child: Container(
+                                    height: 150,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: AppColorsList[index],
+                                      // borderRadius: BorderRadius.only(
+                                      // bottomLeft: Radius.circular(20),
+                                      // bottomRight: Radius.circular(20),
+                                      // ),
+                                      // the color on top of the boxes
+                                    ))),
+                            Positioned(
+                                child: Padding(
+                                    padding: const EdgeInsets.all(15),
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                          child: Text(
+                                            categories[index].name,
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 32,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ))),
+                            Positioned(
+                              height: 150,
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => DetailsPage(),
+                                  ));
+                                },
+                                child: Row(),
                               ),
                             ),
-                                                    
-                          ),
-                          
-                                          
-                        
-                      ],
-                    
-                    ),
-                    
-                  );
-                } 
-                ) 
-            )
-          ],
-        )
-      ),
-
-
+                          ],
+                        ),
+                      ),
+                    );
+                  }))
+        ],
+      )),
     );
-    
-
-    
-    
   }
 }
