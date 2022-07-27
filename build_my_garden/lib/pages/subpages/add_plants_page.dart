20626198849135951f72480a1920d1b903636236
Abemelech <abemelech.mesfin@gmail.com>
2022-07-26 21:10:30 -0500

    Fixed renderflex overflow, fix sign up page, fix add pplants page

--- build_my_garden/lib/pages/subpages/add_plants_page.dart
+++ build_my_garden/lib/pages/subpages/add_plants_page.dart
@@ -65,225 +65,283 @@ class _PlantFormState extends State<PlantForm> {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       backgroundColor: Color.fromARGB(255, 255, 255, 255),
-      body: Container(
-        width: displayWidth(context),
-        margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color.fromARGB(255, 167, 158, 158),
-              ),
-              child: Center(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          width: displayWidth(context),
+          margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromARGB(255, 167, 158, 158),
+                ),
+                child: Center(
+                    child: Container(
                   child: Container(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color.fromARGB(255, 16, 15, 15),
-                  ),
-                  height: 200,
-                  width: 200,
-                  child: GestureDetector(
-                      onTap: () {
-                        plantService
-                            .getImage()
-                            .then((value) => setState(() {}));
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color.fromARGB(255, 229, 222, 222),
-                        ),
-                        height: double.maxFinite,
-                        width: double.maxFinite,
-                        child: plantService.image == null
-                            ? Center(child: Text('No image selected'))
-                            : ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: Image.file(
-                                  File(plantService.image!.path).absolute,
-                                  height: 200,
-                                  width: 200,
-                                  fit: BoxFit.cover,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color.fromARGB(255, 16, 15, 15),
+                    ),
+                    height: 200,
+                    width: 200,
+                    child: GestureDetector(
+                        onTap: () {
+                          plantService
+                              .getImage()
+                              .then((value) => setState(() {}));
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color.fromARGB(255, 229, 222, 222),
+                          ),
+                          height: double.maxFinite,
+                          width: double.maxFinite,
+                          child: plantService.image == null
+                              ? Center(child: Text('No image selected'))
+                              : ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.file(
+                                    File(plantService.image!.path).absolute,
+                                    height: 200,
+                                    width: 200,
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
-                              ),
-                      )),
-                ),
-              )),
-            ),
-            SizedBox(
-              width: 100,
-              height: 10,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                AppText(
-                  text: "Plant Type",
-                  color: Color.fromARGB(255, 37, 36, 36),
-                ),
-                SizedBox(
-                  height: 30,
-                  width: 200,
-                  child: TextField(
-                    controller: _plantTypeController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: const BorderSide(
-                          width: 0,
-                          style: BorderStyle.none,
+                        )),
+                  ),
+                )),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 100,
+                height: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Center(
+                    child: AppText(
+                      text: "Plant Type",
+                      color: Color.fromARGB(255, 37, 36, 36),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                    width: 200,
+                    child: TextField(
+                      controller: _plantTypeController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
                         ),
+                        fillColor: Color.fromARGB(20, 64, 42, 42),
+                        filled: true,
                       ),
-                      fillColor: Color.fromARGB(20, 64, 42, 42),
-                      filled: true,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(
+                    width: 100,
+                    height: 10,
+                  ),
+                  Center(
+                    child: AppText(
+                      text: "Soil Type",
+                      color: Color.fromARGB(255, 59, 56, 56),
                     ),
                   ),
-                )
-              ],
-            ),
-            SizedBox(
-              width: 100,
-              height: 10,
-            ),
-            AppText(
-              text: "Soil Type",
-              color: Color.fromARGB(255, 59, 56, 56),
-            ),
-            SizedBox(
-              height: 30,
-              width: 200,
-              child: TextField(
-                controller: _soilTypeController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      width: 0,
-                      style: BorderStyle.none,
+                  SizedBox(
+                    height: 30,
+                    width: 200,
+                    child: TextField(
+                      controller: _soilTypeController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        fillColor: Color.fromARGB(20, 64, 42, 42),
+                        filled: true,
+                      ),
                     ),
                   ),
-                  fillColor: Color.fromARGB(20, 64, 42, 42),
-                  filled: true,
-                ),
+                ],
               ),
-            ),
-            AppText(
-              text: "Plant's Current Height",
-              color: Color.fromARGB(255, 44, 42, 42),
-            ),
-            SizedBox(
-              height: 30,
-              width: 200,
-              child: TextField(
-                controller: _plantCurrentSizeHeight,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      width: 0,
-                      style: BorderStyle.none,
+              SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: AppText(
+                      text: "Plant's Current Height",
+                      color: Color.fromARGB(255, 44, 42, 42),
                     ),
                   ),
-                  fillColor: Color.fromARGB(20, 64, 42, 42),
-                  filled: true,
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 100,
-              height: 10,
-            ),
-            AppText(
-              text: "Plant's Current Width",
-              color: Color.fromARGB(255, 46, 43, 43),
-            ),
-            SizedBox(
-              height: 30,
-              width: 200,
-              child: TextField(
-                controller: _plantCurrentSizeWidth,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      width: 0,
-                      style: BorderStyle.none,
+                  SizedBox(
+                    height: 30,
+                    width: 200,
+                    child: TextField(
+                      controller: _plantCurrentSizeHeight,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        fillColor: Color.fromARGB(20, 64, 42, 42),
+                        filled: true,
+                      ),
                     ),
                   ),
-                  fillColor: Color.fromARGB(20, 64, 42, 42),
-                  filled: true,
-                ),
+                ],
               ),
-            ),
-            SizedBox(
-              width: 100,
-              height: 10,
-            ),
-            Row(
-              children: [
-                AppText(text: "Plant's Date:"),
-                SizedBox(
-                  width: 5,
-                ),
-                AppText(
-                  text: _date == Null
-                      ? "No date selected"
-                      : "${_date.year}/${_date.month}/${_date.day}",
-                  color: Color.fromARGB(255, 56, 53, 53),
-                  fontWeight: FontWeight.bold,
-                ),
-              ],
-            ),
-            SizedBox(
-              width: 100,
-              height: 10,
-            ),
-            ResponsiveButton(
-              text: "Pick a date",
-              onPress: () {
-                showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(1900),
-                  lastDate: DateTime(2100),
-                ).then((date) => setState(() {
-                      _date = date!;
-                      _plantDated.text = _date.toString();
-                    }));
-              },
-            ),
-            SizedBox(
-              width: 100,
-              height: 10,
-            ),
-            Container(
-              width: displayWidth(context),
-              child: Center(
-                child: ResponsiveButton(
-                  onPress: () async {
-                    var response = await plantService
-                        .uploadPlant(
-                            _plantTypeController.text,
-                            _soilTypeController.text,
-                            _plantCurrentSizeHeight.text,
-                            _plantCurrentSizeWidth.text,
-                            _plantDated.text.split(' ')[0])
-                        .then((value) {
-                      Navigator.pop(context, true);
-                      setState(() {});
-                    });
-                  },
-                  // [To Do] add error handling
-                  text: "Add Plant",
-                ),
+              SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 100,
+                    height: 10,
+                  ),
+                  Center(
+                    child: AppText(
+                      text: "Plant's Current Width",
+                      color: Color.fromARGB(255, 46, 43, 43),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                    width: 200,
+                    child: TextField(
+                      controller: _plantCurrentSizeWidth,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        fillColor: Color.fromARGB(20, 64, 42, 42),
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            )
-          ],
+              SizedBox(height: 20),
+              SizedBox(
+                width: 100,
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 20,
+                    height: 10,
+                  ),
+                  ResponsiveButton(
+                    text: "Pick a date",
+                    onPress: () {
+                      showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(1900),
+                        lastDate: DateTime(2100),
+                      ).then((date) => setState(() {
+                            _date = date!;
+                            _plantDated.text = _date.toString();
+                          }));
+                    },
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  AppText(text: "Plant's Date:"),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  AppText(
+                    text: _date == Null
+                        ? "No date selected"
+                        : "${_date.year}/${_date.month}/${_date.day}",
+                    color: Color.fromARGB(255, 56, 53, 53),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Column(
+                children: [
+                  // SizedBox(
+                  //   width: 20,
+                  //   height: 10,
+                  // ),
+                  // ResponsiveButton(
+                  //   text: "Pick a date",
+                  //   onPress: () {
+                  //     showDatePicker(
+                  //       context: context,
+                  //       initialDate: DateTime.now(),
+                  //       firstDate: DateTime(1900),
+                  //       lastDate: DateTime(2100),
+                  //     ).then((date) => setState(() {
+                  //           _date = date!;
+                  //           _plantDated.text = _date.toString();
+                  //         }));
+                  //   },
+                  // ),
+                  SizedBox(
+                    width: 20,
+                    height: 10,
+                  ),
+                  Container(
+                    width: displayWidth(context),
+                    child: Center(
+                      child: ResponsiveButton(
+                        onPress: () async {
+                          var response = await plantService
+                              .uploadPlant(
+                                  _plantTypeController.text,
+                                  _soilTypeController.text,
+                                  _plantCurrentSizeHeight.text,
+                                  _plantCurrentSizeWidth.text,
+                                  _plantDated.text.split(' ')[0])
+                              .then((value) {
+                            Navigator.pop(context, true);
+                            setState(() {});
+                          });
+                        },
+                        // [To Do] add error handling
+                        text: "Add Plant",
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
