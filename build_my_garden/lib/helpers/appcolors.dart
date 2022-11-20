088b13f9fd624ee06f8483be588a265d02b14ab3
abemelech <abemelech.mesfin@gmail.com>
2022-11-19 19:47:04 -0600

    removed unncessary imports and added constant

--- build_my_garden/lib/helpers/appcolors.dart
+++ build_my_garden/lib/helpers/appcolors.dart
@@ -6,12 +6,11 @@ class AppColors {
   static const Color second = Color.fromARGB(102, 83, 59, 57);
   static const Color third = Color.fromARGB(102, 106, 146, 66);
   static const Color fourth = Color.fromARGB(102, 61, 129, 144);
-
 }
 
 List<Color> AppColorsList = [
-Color.fromARGB(102, 61, 144, 104),
-Color.fromARGB(164, 208, 56, 56),
-Color.fromARGB(102, 106, 146, 66),
-Color.fromARGB(124, 222, 8, 211),
+  const Color.fromARGB(102, 61, 144, 104),
+  const Color.fromARGB(164, 208, 56, 56),
+  const Color.fromARGB(102, 106, 146, 66),
+  const Color.fromARGB(124, 222, 8, 211),
 ];
