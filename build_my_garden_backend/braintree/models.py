8af6c5dcd3f66cb5412157505dbe001044efd951
abemelech <abemelech.mesfin@gmail.com>
2022-12-23 16:03:05 -0600

    created invoicing model and admin

--- build_my_garden_backend/braintree/models.py
+++ build_my_garden_backend/braintree/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Model for Invoices
+class Invoicing(models.Model):
+    
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    transaction_id = models.CharField(max_length=100)
+    amount = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.user}'
