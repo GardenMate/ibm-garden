041eb538d9252f70811a75ccb4233d0e5c950684
abemelech <abemelech.mesfin@gmail.com>
2022-12-24 15:15:42 -0600

    create example view

--- build_my_garden_backend/braintree/views.py
+++ build_my_garden_backend/braintree/views.py
@@ -4,5 +4,88 @@ from django.conf import settings
 from django.http import HttpResponse
 import json
 from braintree.models import Invoicing
+from braintreeAPI import gateway, BraintreeData, BraintreePayment, generate_client_token, transact, find_transaction
 
 # Create your views here.
+
+'''
+Cart view 
+'''
+@login_required
+def cart(request):
+
+    user = request.user
+    ### Need agent_id
+    agent_id = ""
+
+    braintree_client_token = gateway.client_token.generate({"customer_id": agent_id})
+
+    context = {
+        "braintree_client_token": braintree_client_token
+    }
+
+    return render(request, 'api/cart.html', context=context)
+
+'''
+AJAX function to handle Braintree payment
+'''
+@login_required
+def payment(request):
+
+    if request.method == "POST":
+
+        user = request.user
+        token = request.POST.get('braintreeToken', None)
+        card_id = request.POST.get('card_id', None)
+        paymentMethodNonce = request.POST.get('paymentMethodNonce', None)
+        description = request.POST.get("description", None)
+        currency = request.POST.get("currency", None)
+        set_default = request.POST.get("set_default", None)
+
+        amount = request.POST.get('amount')
+
+        ### Need agent id
+        agent_id = ""
+
+        if not agent_id:
+            # Create a braintree account???
+            pass
+        
+        payment = BraintreePayment(
+            user=user,
+            agent_id=agent_id,
+            token=token,
+            card_id=card_id,
+            amount=amount,
+            description=description,
+            currency=currency,
+            set_default=set_default
+        ).create()
+
+        if payment["message"] == "Perfect":
+
+            invoice = Invoicing(
+                user = user,
+                transaction_id = payment["tran_id"],
+                amount = float(amount)
+            )
+
+            invoice.save()
+            user = user
+
+            return HttpResponse(
+                json.dumps({"result": "okay"}),
+                content_type="application/json"
+            )
+        
+        else:
+            return HttpResponse(
+                json.dumps({"result": "error", "message":payment["message"]}),
+                content_type="application/json"
+            )
+    else:
+        return HttpResponse(
+			json.dumps({"result": "error"}),
+			content_type="application/json"
+			)
+
